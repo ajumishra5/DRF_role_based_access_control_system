c33f8d17167a22f3992123e4c96c84545e77f3b9
Ajay Mishra <ajumishra5@gmail.com>
2026-01-06 00:40:28 +0530

    Dashboard designed

---
+++ accounts/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import User
+
+# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
---
+++ accounts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('ADMIN','Admin'),
+        ('MANAGER','Manager'),
+        ('EMPLOYEE','Employee')
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='USER')
+
+    def __str__(self):
+        return f"{self.username}({self.role})"
